4367f7243c7331ee5f7e3b398d0fbc51e7c8d611
evsoncustodio <evsoncustodiooli@gmail.com>
2019-06-12 20:51:55 -0300

    Add files of tools in utils package

---
+++ scripts/utils/bar.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+netmaskBar()
+{
+    bar=""
+
+    for ((i=1;i<5;++i));
+    do
+        bar=$bar$(echo "obase=2;$(echo $netmask | cut -f$i -d.)" | bc)
+    done
+
+    bar=$(echo $bar | grep -o '1' | wc -l)
+}
